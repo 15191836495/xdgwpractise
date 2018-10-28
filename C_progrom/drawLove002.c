6b546859e80559bc64b34549df764e5ec4490ae9
x1a0zheng <44498485+x1a0zheng@users.noreply.github.com>
2018-10-28 22:16:03 +0800

    try001
    C_program1

--- C_progrom/drawLove002.c
+++ C_progrom/drawLove002.c
@@ -4,11 +4,15 @@ int number,k,n;
 
 void main(){
     void draw_top(),draw_bottom();
-    printf("������Ҫ������ͼ����С���ô���1С��17��������ʾ��\n");
+    printf("please input an int to Love's size(from 2 to 18)\n");
+    scanf("%d",&number);
+    k = number;
+    n = 3*(k/2);
+    draw_top();
     draw_bottom();
 }
 
-void draw_top(){//���ε��ϲ���
+void draw_top(){
     for(;k>0;k--,n+=2){
         for(int i=k;i>0;i--)
             printf("%c",0);
@@ -24,3 +28,19 @@ void draw_top(){//���ε��ϲ���
             printf("%c",0);
         printf("\n");
         }
+}
+
+void draw_bottom(){
+    int a = 2*(n+k);
+    int b = 0;
+    for(;a>0;a-=2,b++){
+        for(int i=b;i>0;i--)
+            printf("%c",0);
+        for(int i=a;i>0;i--)
+            printf("%c",42);
+        for(int i=b;i>0;i--)
+            printf("%c",0);
+        printf("\n");
+    }
+}
+
